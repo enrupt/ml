f42a72ea2e356a1c63513665f30ffd6c22483116
kosteeck <enrupt@gmail.com>
2021-01-11 15:35:36 +0300

    week6 fixes

--- coursera/hse_vvedenie/week6/decolor.py
+++ coursera/hse_vvedenie/week6/decolor.py
@@ -24,45 +24,38 @@ def to_rgb(cluster):
     return arr_r, arr_g, arr_b
 
 
-def mean_color(cluster):
+def median_color(cluster):
     cl_r, cl_g, cl_b = to_rgb(cluster)
-    res = [None, None, None]
-    res[0] = np.mean(cl_r)
-    res[1] = np.mean(cl_g)
-    res[2] = np.mean(cl_b)
-    return res
+    mean_res = [None, None, None]
+    mean_res[0] = np.median(cl_r)
+    mean_res[1] = np.median(cl_g)
+    mean_res[2] = np.median(cl_b)
+    return mean_res
 
 
 def avg_color(cluster):
     cl_r, cl_g, cl_b = to_rgb(cluster)
-    res = [None, None, None]
-    res[0] = np.average(cl_r)
-    res[1] = np.average(cl_g)
-    res[2] = np.average(cl_b)
-    return res
+    avg_res = [None, None, None]
+    avg_res[0] = np.average(cl_r)
+    avg_res[1] = np.average(cl_g)
+    avg_res[2] = np.average(cl_b)
+    return avg_res
 
 
-def psnr(original, compressed):
-    mse = np.mean((original - compressed) ** 2)
-    if(mse == 0):
-        return 100
-    max_pixel = np.max(original)
-    psnr = 20 * np.log10(max_pixel / np.sqrt(mse))
-    return psnr
+def mse(original, compressed):
+    return np.mean((original - compressed) ** 2, dtype=np.float64)
+
+
+def psnr(image_true, image_test):
+    err = mse(image_true, image_test)
+    return 10 * np.log10((np.max(image_true) ** 2) / err)
 
 
 image_float = img_as_float(image)
 
-r_arr = image_float[:, :, 0].ravel()
-g_arr = image_float[:, :, 1].ravel()
-b_arr = image_float[:, :, 2].ravel()
-X = []
-y = []
-for i in range(0, len(r_arr)):
-    y.append(i)
-    X.append([r_arr[i], g_arr[i], b_arr[i]])
+X = np.array(image_float.reshape((image_float.shape[0]*image_float.shape[1], 3)))
 
-for n_clusters in range(8,21):
+for n_clusters in range(10, 20):
     kmeans = KMeans(init='k-means++', random_state=241, n_clusters=n_clusters)
     predict = kmeans.fit_predict(X)
 
@@ -73,35 +66,35 @@ for n_clusters in range(8,21):
     for i in range(0, len(predict)):
         clusters[predict[i]].append(X[i])
 
-    mean_cluster_colors = [[]]
+    median_cluster_colors = [[]]
     avg_cluster_colors = [[]]
     for i in range(0, n_clusters):
-        mean_cluster_colors.append([])
+        median_cluster_colors.append([])
         avg_cluster_colors.append([])
 
     for i in range(0, n_clusters):
-        mean_cluster_colors[i] = mean_color(clusters[i])
+        median_cluster_colors[i] = median_color(clusters[i])
         avg_cluster_colors[i] = avg_color(clusters[i])
 
-    X_mean = np.empty((len(X), 3))
+    X_median = np.empty((len(X), 3))
     X_avg = np.empty((len(X), 3))
 
     for i in range(0, len(X)):
-        X_mean[i] = np.asarray(mean_cluster_colors[predict[i]])
+        X_median[i] = np.asarray(median_cluster_colors[predict[i]])
         X_avg[i] = np.asarray(avg_cluster_colors[predict[i]])
 
-    X_mean *= 255
-    X_mean = X_mean.astype(np.uint8)
-    X_mean_3d = np.reshape(X_mean, (height, width, -1))
+    X_median_3d = X_median*255
+    X_median_3d = X_median_3d.astype(np.uint8)
+    X_median_3d = np.reshape(X_median_3d, (height, width, -1))
 
-    X_avg *= 255
-    X_avg = X_avg.astype(np.uint8)
-    X_avg_3d = np.reshape(X_avg, (height, width, -1))
+    X_avg_3d = X_avg*255
+    X_avg_3d = X_avg_3d.astype(np.uint8)
+    X_avg_3d = np.reshape(X_avg_3d, (height, width, -1))
 
-    psnr_mean = psnr(X_mean_3d, image)
-    psnr_avg = psnr(X_avg_3d, image)
-    diff = psnr(X_mean_3d, X_avg_3d)
-    print("clusters", n_clusters, psnr_mean, psnr_avg, diff)
+    psnr_median = psnr(X, X_median)
+    psnr_avg = psnr(X, X_avg)
+    diff = psnr(X_median, X_avg)
+    print("clusters", n_clusters, "psnr_median", psnr_median, "psnr_avg", psnr_avg, mse(X, X_avg))
 
     fig, axes = plt.subplots(1, 3, figsize=(12, 6))
     ax = axes.ravel()
@@ -110,11 +103,11 @@ for n_clusters in range(8,21):
     ax[0].set_axis_off()
 
     ax[1].set_title('Mean picture')
-    ax[1].imshow(X_mean_3d)
+    ax[1].imshow(X_median_3d)
     ax[1].set_axis_off()
 
     ax[2].set_title('Avg picture n_clusters='+str(n_clusters))
     ax[2].imshow(X_avg_3d)
     ax[2].set_axis_off()
 
-    plt.show()
+   # plt.show()
