462dc82c029163dcb106b287490960f050e71d41
Konstantin Bykov <ko.bykov@corp.vk.com>
2021-06-14 19:02:35 +0300

    MIPT MOiAD: course2 week5

--- coursera/mipt_mo_i_ad/course1/week4/test.py
+++ coursera/mipt_mo_i_ad/course1/week4/test.py
@@ -1,5 +1,6 @@
 import numpy as np
-import sklearn.cross_validation
+from sklearn.svm._libsvm import cross_validation
+
 
 def f(k):
     return pow(3, k) * np.math.exp(-3)/ np.math.factorial(k);
@@ -8,4 +9,5 @@ p = 1;
 for k in range(0, 5):
     p -= f(k);
 
+cross_validation
 print(p);
